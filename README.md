b82a7b33087de0566521ab0e1b05b1203a4920f2
phxie <teemoisholy@gmail.com>
2025-04-22 12:24:05 -0700

    YAML files for 2 categories

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Sample command line to download 10 arXiv Hardware Architecture papers from 2024 
 uv run .\paper_downloading\download_arxiv.py -f yaml --output-dir arxiv_papers -c AR -n 10
 ```
 
-Sample command line to download arXiv Computer Science paper PDFs from February 2023 to June 2023
+Sample command line to download arXiv Computer Science paper PDFs from February 1st 2023 to June 1st 2023
 
 ```
 uv run .\paper_downloading\download_arxiv.py --output-dir arxiv_papers --start-date 20230201 --end-date 20230601
---
+++ clustering/ACM_yaml_generator.py
@@ -0,0 +1,81 @@
+import yaml
+import os
+import argparse
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+def update_yaml(url, output_file):
+    """
+    Scrapes an ACM digital library article page for paper information and stores in a yaml file.
+
+    Args:
+        url: ACM library article url
+        output_file: Existing yaml file to update
+    
+    """
+    if not os.path.exists(output_file):
+        open(output_file, 'w').close()
+
+    with open(output_file,'r') as yamlfile:
+        yaml_content = yaml.safe_load(yamlfile)
+
+    if not yaml_content:
+        yaml_content = {}
+
+    if not yaml_content.get('papers', None):
+        yaml_content['papers'] = []
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
+
+    dr = webdriver.Chrome(options=options)
+    dr.get(url)
+    page = BeautifulSoup(dr.page_source,"lxml")
+
+    references = []
+    for ref in page.find_all("div", attrs={"class":"citation-content"}):
+        references.append(ref.text)
+
+    references = " ".join(references)
+
+    title = page.find("div", attrs={"class": "core-publication-title"}).text
+
+    abstract_section = page.find("section", attrs={"property":"abstract"})
+    abstract = abstract_section.find("div", attrs={"role":"paragraph"}).text
+
+    keywords_section = page.find("section", attrs={"id":"sec-terms"})
+    keywords_elements = keywords_section.find_all("a")
+    keywords = " ".join([k.text for k in keywords_elements])
+
+    new_paper = {'abstract': abstract,
+                    'keywords': keywords,
+                    'title': title,
+                    'url': url,
+                    'references': references
+                }
+
+    yaml_content['papers'].append(new_paper)
+
+    dr.close()
+
+    with open(output_file,'w') as yamlfile:
+        yaml.safe_dump(yaml_content, yamlfile)
+
+    print(f"# of papers in YAML: {len(yaml_content['papers'])}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download arXiv CS papers as PDFs or Markdown.")
+    parser.add_argument(
+        "--output-file", "-o", type=str, required=True,
+        help="Yaml file to update"
+    )
+    args = parser.parse_args()
+
+    while True:
+        url = input('Enter an ACM digital library article url (Enter DONE to stop):\n')
+        if url == "DONE":
+            break
+        update_yaml(url, args.output_file)
+
+
