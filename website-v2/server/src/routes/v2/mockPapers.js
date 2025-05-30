1391d64f2159974b8981a1d409b8440b36eeddbe
Devanathan N G <ngdeva99@gmail.com>
2025-05-30 16:30:56 -0700

    no op code remove

--- website-v2/server/src/routes/v2/mockPapers.js
+++ website-v2/server/src/routes/v2/mockPapers.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 
 // Import the paper generation functions
-const { generatePaper, generateAbstract, generateTitle, generateURL, generateArxivId } = require('../../../../generate-batch-mock-papers')
+const { generatePaper, generateAbstract, generateTitle, generateURL, generateArxivId } = require('../../../../scripts/generate-batch-mock-papers')
 const routes = [
     {
         method: 'POST',
